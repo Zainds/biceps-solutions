40422a509cf87e8684872c344cbf9f6623116ea4
Zainds <beat1097@mail.ru>
2025-05-12 20:41:44 +0700

    Move supabase keys to .env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,7 +3,7 @@ import type { Database } from './database.types';
 
 // We need to get these from environment variables
 // For now we'll use temporary dummy values
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://ieogbiucxmlfeedamgij.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imllb2diaXVjeG1sZmVlZGFtZ2lqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY5NzgxNDIsImV4cCI6MjA2MjU1NDE0Mn0.FPvqvvOYYXT-5HJOrpBZcJRN6gHIshKIQNkOIQJ3lfs';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
